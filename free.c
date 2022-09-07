d8eb27eb201f3a6baaaf0d351361786e90e284a0
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-09-07 12:14:34 +0200

    Add project files

---
+++ free.c
@@ -0,0 +1,84 @@
+#include "monty.h"
+#include "lists.h"
+
+/**
+ * free_all - custom memory handling function
+ * @all: flag to indicate what to free
+ */
+void free_all(int all)
+{
+	if (data.line)
+	{
+		free(data.line);
+		data.line = NULL;
+		free_everything(data.words);
+		data.words = NULL;
+	}
+
+	if (all)
+	{
+		if (data.stack)
+		{
+			free_dlistint(data.stack);
+			data.stack = NULL;
+		}
+		if (data.fptr)
+		{
+			fclose(data.fptr);
+			data.fptr = NULL;
+		}
+	}
+}
+
+/**
+ * pchar_handler - handles the pchar instruction
+ * @stack: double pointer to the stack to push to
+ * @line_number: number of the line in the file
+ */
+void pchar_handler(stack_t **stack, unsigned int line_number)
+{
+	stack_t *node = *stack;
+
+	if (!node)
+	{
+		dprintf(STDERR_FILENO, PCHAR_FAIL, line_number);
+		free_all(1);
+		exit(EXIT_FAILURE);
+	}
+
+	if (node->n < 0 || node->n > 127)
+	{
+		dprintf(STDERR_FILENO, PCHAR_RANGE, line_number);
+		free_all(1);
+		exit(EXIT_FAILURE);
+	}
+
+	putchar(node->n);
+	putchar('\n');
+}
+
+/**
+ * pstr_handler - handles the pstr instruction
+ * @stack: double pointer to the stack to push to
+ * @line_number: number of the line in the file
+ */
+void pstr_handler(stack_t **stack, unsigned int line_number)
+{
+	stack_t *node = *stack;
+
+	(void)line_number;
+
+	if (!node)
+	{
+		putchar('\n');
+		return;
+	}
+
+	while (node && node->n != 0 && node->n >= 0 && node->n <= 127)
+	{
+		putchar(node->n);
+		node = node->next;
+	}
+
+	putchar('\n');
+}
